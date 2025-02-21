d8dfad683d3fe14230d103ad13c5b8fbbcc79a92
hmmasudsarder <hmmasudsardar@gmail.com>
2025-02-21 16:32:20 +0600

    Authentication added successfully

---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useUser } from "@/context/UserContext";
+
+
+const HomePage = () => {
+  const user = useUser();
+  console.log(user)
+  return (
+    <div>
+      <h1>Welcome To NextMart Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
