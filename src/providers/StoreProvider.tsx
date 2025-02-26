ef8d96ed70f874ebf4c4f33e80ce97b54960cef2
hmmasudsarder <hmmasudsardar@gmail.com>
2025-02-26 14:20:14 +0600

    coupon funconality added

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
+import Loading from "@/components/ui/loading";
 import { AppStore, makeStore } from "@/redux/store";
 import { ReactNode, useRef } from "react";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function StoreProvider({ children }: { children: ReactNode }) {
   const storeRef = useRef<AppStore>(undefined);
@@ -11,5 +16,13 @@ export default function StoreProvider({ children }: { children: ReactNode }) {
     storeRef.current = makeStore();
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  const persistedStore = persistStore(storeRef.current);
+
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={<Loading />} persistor={persistedStore}>
+      {children}
+      </PersistGate>
+    </Provider>
+  );
 }
